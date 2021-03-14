630aef8c9da72edab07ca82f62eb38d875836b88
haya_dia <hirovabo@yahoo.co.jp>
2021-03-14 23:54:28 +0900

    Add counter button

--- app/src/main/java/com/example/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasicscodelab/MainActivity.kt
@@ -5,11 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,10 +38,12 @@ fun MyApp(content: @Composable () -> Unit) {
 @Composable
 fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
     Column {
-        for (name in names){
+        for (name in names) {
             Greeting(name)
             Divider(color = Color.Black)
         }
+        Divider(color = Color.Transparent, thickness = 32.dp)
+        Counter()
     }
 }
 
@@ -53,6 +54,15 @@ fun Greeting(name: String) {
             text = "Hello $name!",
             modifier = Modifier.padding(24.dp)
         )
+    }
+}
+
+@Composable
+fun Counter() {
+    val count = remember { mutableStateOf(0) }
+
+    Button(onClick = { count.value++ }) {
+        Text("I've been clicked ${count.value} times")
     }
 }
 
